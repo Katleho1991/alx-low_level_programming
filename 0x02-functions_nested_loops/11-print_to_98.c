7878fbbdfdc81ac3590056ccc7e4cc250918c1e1
Katleho1991 <katlehomazibuko@gmail.com>
2024-06-26 21:01:01 -0700

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (;n <= 98; n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
-	printf("%d",n);
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
@@ -33,7 +33,7 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printf("%d",n);
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
