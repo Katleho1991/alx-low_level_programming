ca180f79a54c0c65864ffc6c57091c1169e245b7
Katleho1991 <katlehomazibuko@gmail.com>
2024-07-01 14:30:32 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - this is the function
+ * @a: the first integer
+ * @b: the second integer
+ */
+
+void swap_int(int *a, int *b)
+
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
