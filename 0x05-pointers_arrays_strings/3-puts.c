583c3691bdd1ea422586e9b7618f0aaf1d9f86b4
Katleho1991 <katlehomazibuko@gmail.com>
2024-07-01 15:18:47 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - this is the name of the function
+ * @str: this is the function parameter
+ */
+
+void _puts(char *str)
+
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+_putchar (str[i]);
+_putchar ('\n');
+}
