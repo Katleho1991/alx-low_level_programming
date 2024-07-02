cbd373f4f99eae7bf150c9136213fd297246c385
Katleho1991 <katlehomazibuko@gmail.com>
2024-07-01 17:35:06 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - this is the main function
+ * @str: this is the function parameter
+ */
+
+void puts2(char *str)
+
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (i % 2 == 0)
+		_putchar(str[i]);
+}
+_putchar('\n');
+}
